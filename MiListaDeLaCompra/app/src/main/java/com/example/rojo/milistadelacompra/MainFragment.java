5fa2f909840a65acb9b9757f146e4cac12f9edac
Rojo97 <victorrojo1997@gmail.com>
2019-01-10 20:57:12 +0100

    Conexion con la DB

--- MiListaDeLaCompra/app/src/main/java/com/example/rojo/milistadelacompra/MainFragment.java
+++ MiListaDeLaCompra/app/src/main/java/com/example/rojo/milistadelacompra/MainFragment.java
@@ -3,7 +3,9 @@
 package com.example.rojo.milistadelacompra;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
 
 public class MainFragment extends Fragment implements View.OnClickListener{
 
@@ -19,6 +31,10 @@ public class MainFragment extends Fragment implements View.OnClickListener{
     private SimpleCursorAdapter mAdapter;
     private static final String[] FROM = {StatusContract.Column.USER, StatusContract.Column.MESSAGE, StatusContract.Column.CREATED_AT};
     private static final int LOADER_ID = 42;
+    private static final String url = "jdbc:mysql://virtual.lab.inf.uva.es:20064/listaCompra";
+    private static final String user = "root";
+    private static final String pass = "";
+    String res;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +62,8 @@ public class MainFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         int i = view.getId();
+        ConnectMySql connectMySql = new ConnectMySql(this.getView(), this.getActivity());
+        connectMySql.execute("");
         Log.d(TAG,"OnClicked " + i);
         Intent intentLista = new Intent(view.getContext(), ListaActivity.class);
         Bundle bundle = new Bundle();
@@ -53,4 +71,54 @@ public class MainFragment extends Fragment implements View.OnClickListener{
         intentLista.putExtras(bundle);
         startActivity(intentLista);
     }
+
+    private class ConnectMySql extends AsyncTask<String, Void, String> {
+        String res = "";
+
+        Context contexto;
+        View view;
+        public ConnectMySql(View view, Context contexto){
+            this.view = view;
+            this.contexto = contexto;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(contexto, "Please wait...", Toast.LENGTH_SHORT)
+                    .show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                System.out.println("Databaseection success");
+
+                String result = "Database Connection Successful\n";
+                Statement st = con.createStatement();
+                st.execute("INSERT INTO ListaCompra VALUES ('listaDeLaApp', 'victor');");
+                //ResultSet rs = st.executeQuery("select * from Usuario");
+
+                //ResultSetMetaData rsmd = rs.getMetaData();
+
+                //while (rs.next()) {
+                //    result += rs.getString(1) + "\n";
+                //}
+                //res = result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                res = e.toString();
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.e(TAG, result);
+        }
+    }
+
 }
